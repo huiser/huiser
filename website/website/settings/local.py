ecffe0824267bc52cb75d23f0a4320bf9e7d2ae0
Stephan Huiser <stephan@huiser.nl>
2013-04-27 15:03:04 +0200

    Set development db parameters.

--- website/website/settings/local.py
+++ website/website/settings/local.py
@@ -5,19 +5,18 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 EMAIL_HOST = "localhost"
-EMAIL_PORT = 1025
-.
-DATABASES = {
-    {
-        "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "twoscoops",
-        "USER": "",
-        "PASSWORD": "",
-        "HOST": "localhost",
-        "PORT": "",
-    }
-}
+EMAIL_PORT = 25
+#DATABASES = {
+#    {
+#        "default": {
+#            "ENGINE": "django.db.backends.postgresql_psycopg2",
+#            "NAME": "huiser",
+#            "USER": "huiser",
+#            "PASSWORD": "huiser",
+#            "HOST": "localhost",
+#            "PORT": "",
+#    }
+#}
 INSTALLED_APPS += ("debug_toolbar", )
 INTERNAL_IPS = ("127.0.0.1",)
 MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware", )
